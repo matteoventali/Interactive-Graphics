96f7edadaed234796a708dc2022bca151c048231
Matteo <matteo@DESKTOP-A44G1DR.broadband>
2025-03-23 12:01:53 +0100

    Project2 implemented

--- project2/project2.js
+++ project2/project2.js
@@ -3,7 +3,22 @@
 // The given rotation value is in degrees.
 function GetTransform( positionX, positionY, rotation, scale )
 {
-	return Array( 1, 0, 0, 0, 1, 0, 0, 0, 1 );
+	// Get radiants from degrees
+	r = rotation * Math.PI/180;
+	
+	// Build of translation matrix
+	translationMatrix = Array(1, 0, 0, 0, 1, 0, positionX, positionY, 1);
+
+	// Build of rotation matrix (counter-clockwise)
+	rotationMatrix = Array(Math.cos(r), Math.sin(r), 0, -Math.sin(r), Math.cos(r), 0, 0, 0, 1);
+
+	// Build of scale matrix
+	scaleMatrix = Array(scale, 0, 0, 0, scale, 0, 0, 0, 1);
+	
+	// Matrix transformation result
+	result = ApplyTransform(ApplyTransform(scaleMatrix, rotationMatrix), translationMatrix);
+
+	return result;
 }
 
 // Returns a 3x3 transformation matrix as an array of 9 values in column-major order.
@@ -11,5 +26,24 @@ function GetTransform( positionX, positionY, rotation, scale )
 // The returned transformation first applies trans1 and then trans2.
 function ApplyTransform( trans1, trans2 )
 {
-	return Array( 1, 0, 0, 0, 1, 0, 0, 0, 1 );
+	// To apply first trans1 and then trans2 we have to compute the product trans2 * trans1
+	result = Array(9).fill(0);
+	
+	for(row = 0; row < 3; row++) // row of trans2
+	{
+		for(column = 0; column < 3; column++) // column of trans1
+		{
+			partialSum = 0;
+			
+			for (j=0; j < 3; j++) // Internal index in the scalar-product
+			{
+				partialSum += trans2[row + j*3] * trans1[column*3 + j];
+			}
+
+			// Store the partial sum into the result array
+			result[column*3 + row] = partialSum;
+		}
+	}
+
+	return result;
 }
